8d071c8a9cd3aa4966af2677592d9f7a0f38d48b
Sajjon <alex.cyon@gmail.com>
2018-08-29 09:03:29 +0200

    adding macOS targets

---
+++ Source/SupportingFiles/macOS/EquationKit_macOS.h
@@ -0,0 +1,19 @@
+//
+//  EquationKit_macOS.h
+//  EquationKit_macOS
+//
+//  Created by Alexander Cyon on 2018-08-28.
+//  Copyright © 2018 Sajjon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EquationKit_macOS.
+FOUNDATION_EXPORT double EquationKit_macOSVersionNumber;
+
+//! Project version string for EquationKit_macOS.
+FOUNDATION_EXPORT const unsigned char EquationKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EquationKit_macOS/PublicHeader.h>
+
+
